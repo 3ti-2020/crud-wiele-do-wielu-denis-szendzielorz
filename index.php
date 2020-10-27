b6f42ec6321f63480f02c43a030462fbec88173c
Denis Sz <62932767+VokuN77@users.noreply.github.com>
2020-10-27 15:05:42 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Denis Szendzielorz</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
-        <div class="item a"></div>
+        <div class="item a">
+            <h1>Denis Szendzielorz</h1>
+        </div>
 
 
             <div class="item b"></div>
