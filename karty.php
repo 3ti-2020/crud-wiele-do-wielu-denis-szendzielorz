6fa83fc1c5df823ca8e386402404033a3b88f68c
Denis Sz <62932767+VokuN77@users.noreply.github.com>
2020-11-04 22:18:57 +0100

    Add files via upload

---
+++ karty.php
@@ -0,0 +1,71 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['logged']))
+        header('Location: log.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+    <div class="karta">
+        <div class="obr1"></div>
+
+        <div class="text">
+            <h2>BMW</h2>
+            <p>Opis</p>
+        </div>
+
+        <div class="stats">
+
+        <div class="stat">
+            <div class="wart">374</div>
+            <div class="typ">KM</div>
+        </div>
+
+        <div class="stat">
+            <div class="wart">630 000</div>
+            <div class="typ">PLN</div>
+        </div>
+
+        <div class="stat">
+            <div class="wart">250 KM/H</div>
+            <div class="typ">VMAX</div>
+        </div>
+    </div>
+    </div>
+
+
+    <div class="karta">
+    <div class="obr2"></div>
+
+        <div class="text">
+            <h2>Maserati</h2>
+            <p>Opis</p>
+        </div>
+
+        <div class="stats">
+
+        <div class="stat">
+            <div class="wart">580</div>
+            <div class="typ">KM</div>
+        </div>
+
+        <div class="stat">
+            <div class="wart">162 000</div>
+            <div class="typ">EUR</div>
+        </div>
+
+        <div class="stat">
+            <div class="wart">326 KM/H</div>
+            <div class="typ">VMAX</div>
+        </div>
+    </div>
+</div>  
+
+</body>
+</html>
---
+++ logowanie.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+
+$servername = 'sql7.freemysqlhosting.net';
+$username = 'sql7373150';
+$password = 'gwQeUeTr6F';
+$dbname = 'sql7373150';
+
+$conn= new mysqli($servername,$username,$password,$dbname);
+
+if(isset($_POST['username'])){
+    $username = htmlentities($_POST['username']);
+    $password = htmlentities($_POST['password']);
+    
+    $sql = "SELECT * from users WHERE username='$username' AND password='$password'";
+
+    $result = $conn->query($sql);
+    if($result)
+        $_SESSION['logged'] = true;
+    else
+        $error = true;
+}
+
+if(isset($_SESSION['logged']))
+    header('Location: karty.php');
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Denis Szendzielorz gr2</title>
+    <link rel="stylesheet" href="logowanie.css">
+</head>
+<body>
+    <form method="post" class="log">
+        <label for="username">Nazwa uzytkownika:</label>
+        <input type="text" name="username">
+
+        <label for="password">Haslo:</label>
+        <input type="password" name="password">
+
+        <input type="submit" value="Zaloguj ">
+
+
+        <?php
+
+        if(isset($error) && $error = true){
+            echo "<span class='blad'>Zaloguj sie ponownie</span>";
+            unset($error);
+        }
+        ?>
+
+    </form>
+</body>
+</html>
