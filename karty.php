29658a2e3489da24ba694a99aa7952ab204516c5
Denis Sz <62932767+VokuN77@users.noreply.github.com>
2020-11-04 22:22:42 +0100

    Add files via upload

--- karty.php
+++ karty.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(!isset($_SESSION['logged']))
-        header('Location: log.php');
+        header('Location: logowanie.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
