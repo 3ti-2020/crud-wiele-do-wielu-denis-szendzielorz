23d1523f395d25c648979cc8fa6ae0ae15b43e7d
Denis Sz <62932767+VokuN77@users.noreply.github.com>
2020-11-09 22:57:43 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const num = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
 const Btn = document.querySelector('.Btn');
 
-const Bcg = document.querySelector('body');
+const Bcg = document.querySelector('.container');
 
 const hex = document.querySelector('.hex');
 
