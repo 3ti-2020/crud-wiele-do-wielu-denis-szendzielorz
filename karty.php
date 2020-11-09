a74616e2aefb1bf284d5f84a116de917b7dada1e
Denis Sz <62932767+VokuN77@users.noreply.github.com>
2020-11-09 20:39:50 +0100

    Add files via upload

--- karty.php
+++ karty.php
@@ -14,7 +14,7 @@
 <body>
 <div class="container">
 <div class="item a">
-<a class="aa" href="wyloguj.php">WYLOGUJ_SIE</a>
+<a class="aa" href="wyloguj.php">WYLOGUJ SIE</a>
 </div>
     <div class="item b">
     <div class="karta">
